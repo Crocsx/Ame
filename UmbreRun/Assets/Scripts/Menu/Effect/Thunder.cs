fb6e4efb935f2b9fd3cfb2191adb5539ce3eddcf
Crocsx <Crocsx.gaming@gmail.com>
2018-08-28 03:45:50 +0900

    Thunder

---
+++ UmbreRun/Assets/Scripts/Menu/Effect/Thunder.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thunder : MonoBehaviour {
+
+    public Material[] thunderMaterial;
+    ParticleSystemRenderer pSystem;
+	// Use this for initialization
+	void Start () {
+        pSystem = GetComponent<ParticleSystemRenderer>();
+    }
+	
+	// Update is called once per frame
+	void PlayThunder () {
+        pSystem.material = thunderMaterial[Random.Range(0, thunderMaterial.Length - 1)];
+    }
+}
