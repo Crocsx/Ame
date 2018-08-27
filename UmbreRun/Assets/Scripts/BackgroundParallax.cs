c8a248cc7854208115171f8b6927ba4f397ee532
Maxime ROUFFET <m.rouffet@student.isartdigital.com>
2018-08-27 22:03:51 +0900

    [Fix] Scrolling Background

--- UmbreRun/Assets/Scripts/BackgroundParallax.cs
+++ UmbreRun/Assets/Scripts/BackgroundParallax.cs
@@ -3,33 +3,37 @@
 public class BackgroundParallax : MonoBehaviour
 {
     [SerializeField]
-    SpriteRenderer m_background1;
-
-    [SerializeField]
-    SpriteRenderer m_background2;
+    SpriteRenderer m_background;
 
     [SerializeField]
     float m_scrollSpeed = 2.0f;
 
     float m_currTime = 0.0f;
 
+    //Bad Hardcode
+    float m_width = 19.0f;
+
+    Vector3 m_startPos;
+    Vector3 m_endPos;
+
+    private void Start()
+    {
+        m_startPos = m_background.transform.position;
+        m_endPos = m_startPos - new Vector3(m_width, 0.0f, 0.0f);
+    }
+
     void Update ()
     {
-        float width = 17;
+        //m_background.transform.position -= new Vector3(m_scrollSpeed * Time.deltaTime, 0.0f, 0.0f);
+
         m_currTime += Time.deltaTime * m_scrollSpeed;
 
-        if (m_currTime > width)
-            m_currTime -= width;
+        m_background.transform.position = Vector3.Lerp(m_startPos, m_endPos, m_currTime);
 
-        Vector3 offset = new Vector3(m_currTime, 0.0f, 0.0f);
-
-        m_background1.transform.position -= offset;
-        m_background2.transform.position -= offset;
-
-        if (m_background1.transform.position.x <= -width)
-            m_background1.transform.position = m_background2.transform.position + new Vector3(width + 2, 0.0f, 0.0f);
-
-        else if (m_background2.transform.position.x <= -width)
-            m_background2.transform.position = m_background1.transform.position + new Vector3(width + 2, 0.0f, 0.0f);
+        if (m_currTime >= 1.0f)
+        {
+            m_background.transform.position += new Vector3(m_width, 0.0f, 0.0f);
+            m_currTime--;
+        }
     }
 }
