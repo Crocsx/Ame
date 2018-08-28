8ea64201268e6927e6b379bd1127b899b5c2c7a3
Maxime ROUFFET <m.rouffet@student.isartdigital.com>
2018-08-28 17:42:20 +0900

    [Add] BootMgr

---
+++ UmbreRun/Assets/Scripts/BootMgr.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BootMgr : MonoBehaviour
+{
+    [SerializeField]
+    GameObject GameMgr;
+
+    [SerializeField]
+    GameObject SoundMgr;
+
+    void Start ()
+    {
+        Instantiate(GameMgr);
+        Instantiate(SoundMgr);
+
+        SceneManager.LoadScene("Menu");
+    }
+}
