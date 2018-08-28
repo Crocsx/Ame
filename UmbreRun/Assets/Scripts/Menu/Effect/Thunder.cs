1b1a9dba49f01f885d130546e372f54394df8e20
Crocsx <Crocsx.gaming@gmail.com>
2018-08-28 13:11:02 +0900

    Thunder

--- UmbreRun/Assets/Scripts/Menu/Effect/Thunder.cs
+++ UmbreRun/Assets/Scripts/Menu/Effect/Thunder.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Thunder : MonoBehaviour {
 
+    public AudioClip[] thunderSound;
     public Material[] thunderMaterial;
 
 	public void Play () {
         GetComponent<ParticleSystemRenderer>().material = thunderMaterial[Random.Range(0, thunderMaterial.Length - 1)];
+        GetComponent<AudioSource>().clip = thunderSound[Random.Range(0, thunderSound.Length - 1)];
+        GetComponent<AudioSource>().Play();
         transform.GetComponent<ParticleSystem>().Play();
     }
 }
