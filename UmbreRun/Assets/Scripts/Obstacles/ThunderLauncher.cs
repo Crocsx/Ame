7a22845f0423cfd1a9a2438e2172b41b26fbc55c
Crocsx <Crocsx.gaming@gmail.com>
2018-08-28 15:25:23 +0900

    hum

---
+++ UmbreRun/Assets/Scripts/Obstacles/ThunderLauncher.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThunderLauncher : MonoBehaviour {
+
+    Thunder thunder;
+
+    float TIME_MIN = 6;
+    float TIME_MAX = 12;
+    float timer;
+    float targetTimer;
+    // Use this for initialization
+    void Start () {
+        thunder = GetComponent<Thunder>();
+        timer = 0;
+    }
+	
+    void nextBolt()
+    {
+        targetTimer = Random.Range(TIME_MIN, TIME_MAX);
+        timer = 0;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(timer);
+
+        timer += Time.deltaTime;
+        if(timer > targetTimer)
+        {
+            thunder.Play();
+            nextBolt();
+        }
+    }
+}
